8d429126be080239356a4deeef69413b195dee04
Chris <chris_s_botha@hotmail.com>
2016-11-26 17:17:54 -0500

    aaa

---
+++ WindowsApp1/Class1.vb
@@ -0,0 +1,7 @@
+﻿Public Class Class1
+
+	Public Function CreateTheOther() As Class2
+		Return New Class2()
+	End Function
+
+End Class
--- WindowsApp1/Form1.vb
+++ WindowsApp1/Form1.vb
@@ -1,5 +1,7 @@
 ﻿Public Class Form1
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        MsgBox("The button was pressed")
-    End Sub
+
+	Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+		MsgBox("The button was pressed")
+	End Sub
+
 End Class
